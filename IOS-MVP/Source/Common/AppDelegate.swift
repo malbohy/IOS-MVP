7f78646b0bafd557f56164570c928d1dc73229da
mohamed elbohy <malbohy@gmail.com>
2019-11-30 14:10:27 +0200

    copy MVC Code

---
+++ IOS-MVP/Source/Common/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  MVC-IOS
+//
+//  Created by mohamed albohy on 11/30/17.
+//  Copyright © 2017 Mohamed Elbohy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    let favoriteModel = FavoriteModel()
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if let viewControllers = (window?.rootViewController as? UITabBarController)?.viewControllers {
+            for value in viewControllers.enumerated() {
+                switch value {
+                case let (0, nc as UINavigationController):
+                    let searchVC = SearchViewController(searchModel: .init(), favoriteModel: favoriteModel)
+                    nc.setViewControllers([searchVC], animated: false)
+
+                case let (1, nc as UINavigationController):
+                    let favoriteVC = FavoriteViewController(favoriteModel: favoriteModel)
+                    nc.setViewControllers([favoriteVC], animated: false)
+
+                default:
+                    continue
+                }
+            }
+        }
+        
+        return true
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'IOS-MVP' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for IOS-MVP
+  pod 'GithubKit', :git => 'https://github.com/marty-suzuki/GithubKitForSample.git', :tag => '0.4.1'
+  pod 'NoticeObserveKit', '0.11.1'
+  pod 'Nuke'
+  pod 'RxAtomic'
+  pod 'RxCocoa'
+  pod 'RxSwift'
+  pod 'SwiftIconFont'
+  
+end
